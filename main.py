96026ac0a79d8bf8737d1a7824934d9da5cd1e4e
YoavCarmel <carmel.yoav@gmail.com>
2021-01-06 14:58:40 +0200

    fixed problem 206

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
 import time
 
-from problems.P120 import ans
+from problems.P206 import ans
+from solution_file.solution_file_creator import add_problem_solution
 
 start_time = int(round(time.time() * 1000))
 print(ans())
-# add_problem_solution(118, override=True)
+# add_problem_solution(206, override=True)
 print((int(round(time.time() * 1000)) - start_time) / 1000, "seconds")
--- problems/P206.py
+++ problems/P206.py
@@ -1,40 +1,57 @@
 from typing import Set
 
 
-def ans():  # really bad solution based on the fact that i already know the answer.
+def ans():
     pattern = "1_2_3_4_5_6_7_8_9_0"
     pattern = pattern[::-1]
-    curr_layer: Set[str] = {"0"}
-    for _ in range(8):
-        next_layer = set()
-        for n in curr_layer:
-            for i in range(10):
-                s = str(i) + n
-                if match_pattern(pattern, int(s) ** 2) and len(s) <= len(pattern) // 2 + 2:
-                    next_layer.add(s)
-        curr_layer = next_layer
-    for n in curr_layer:
-        for i in range(1, 20):
-            s = str(i) + n
-            if match_pattern_exactly(pattern, int(s) ** 2):
-                return s
+    # only the first i digits of n affect the first i digits of n**2.
+    # first digit must be 0, but then add 2 digits each time and see if matcehs the next number in pattern
+    possible_n: Set[int] = set()
+    possible_n.add(0)
+    level = 1
+    while True:
+        # continue addign numbers that match
+        next_level: Set[int] = set()
+        for n in possible_n:
+            for i in range(1, 100):
+                # add the digits to the left
+                new_n = n + i * (10 ** (2 * level - 1))
+                new_n_squared_str_reversed = str(new_n ** 2)[::-1]
+                if len(new_n_squared_str_reversed) > len(pattern):
+                    # do not continue with bigger numbers
+                    break
+                if matches_pattern(pattern, new_n_squared_str_reversed, level):
+                    # if after the addition it still matches the pattern, add to next level
+                    next_level.add(new_n)
+                if perfect_match(pattern, new_n_squared_str_reversed):
+                    # if perfect match, return. we got the answer
+                    return new_n
+        possible_n = next_level
+        level += 1
 
 
-def match_pattern(pattern_rev: str, num: str):
-    s = str(num)[::-1]
-    for i in range(len(s) // 2):
-        if pattern_rev[i] != "_":
-            if s[i] != pattern_rev[i]:
-                return False
+def matches_pattern(pat: str, num: str, level: int):
+    """
+    :param pat: pattern to match
+    :param num: the num to check if matches
+    :param level: number of levels in pattern to match
+    :return: True if num matches pattern for this level
+    """
+    for i in range(0, level * 2 + 1, 2):
+        if pat[i] != num[i]:
+            return False
     return True
 
 
-def match_pattern_exactly(pattern_rev: str, num: str):
-    s = str(num)[::-1]
-    if len(pattern_rev) != len(s):
+def perfect_match(pat: str, num: str):
+    """
+    :param pat: a pattern to match
+    :param num: a number to check if matches
+    :return: True if the number matches completely the pattern, and this is the result of the problem
+    """
+    if len(num) != len(pat):
         return False
-    for i in range(len(s)):
-        if pattern_rev[i] != "_":
-            if s[i] != pattern_rev[i]:
-                return False
+    for i in range(0, len(pat) + 1, 2):
+        if pat[i] != num[i]:
+            return False
     return True
