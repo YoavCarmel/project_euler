853e36f37c3db3b034910b41ade356272c1fa2b0
YoavCarmel <carmel.yoav@gmail.com>
2021-09-04 17:40:53 +0300

    fixed type of solution file runtime column

--- main.py
+++ main.py
@@ -4,6 +4,6 @@ from problems.P165 import ans
 from solution_file.solution_file_creator import add_problem_solution
 
 start_time = int(round(time.time() * 1000))
-# print(ans())
-add_problem_solution(165, override=True)
+print(ans())
+# add_problem_solution(165, override=True)
 print((int(round(time.time() * 1000)) - start_time) / 1000, "seconds")
