52f2f842c23b6718633c30262cfbf98999ded6ad
YoavCarmel <carmel.yoav@gmail.com>
2021-09-14 00:39:40 +0300

    created an analysis package, and moved P700 analysis into it

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import time
 
-from problems.P700 import ans
+from problems.P686 import ans
 from solution_file.solution_file_creator import add_problem_solution
 
 start_time = int(round(time.time() * 1000))
