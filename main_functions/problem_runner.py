d3c26ad9fe847609430a0252ab0795a9dfe5e973
YoavCarmel <carmel.yoav@gmail.com>
2021-12-21 16:42:12 +0200

    added run_all function

--- main_functions/problem_runner.py
+++ main_functions/problem_runner.py
@@ -1,6 +1,12 @@
+import sys
+
+import pandas as pd
 import time
 
+from tqdm import tqdm
+
 from main_functions.runs_helper import get_import_line, get_run_line
+from main_functions.solution_file_handler import PROBLEM_NUM
 
 
 def run(problem_number):
@@ -8,3 +14,14 @@ def run(problem_number):
     start_time = time.time()
     print(eval(get_run_line(problem_number)))
     print(round(time.time() - start_time, 3), "seconds")
+
+
+def run_all():
+    all_problems = list(pd.read_csv("solution_file.csv")[PROBLEM_NUM])
+    for problem_num in tqdm(all_problems):
+        print(f"{problem_num=}")
+        try:
+            run(problem_num)
+        except Exception as e:
+            print("ERROR", e)
+        print()
