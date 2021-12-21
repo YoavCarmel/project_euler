61de6cf194158fefd80a97a25b824b5111f45944
YoavCarmel <carmel.yoav@gmail.com>
2021-12-21 19:16:42 +0200

    improved pickle_help.py

--- libs/pickle_help.py
+++ libs/pickle_help.py
@@ -6,9 +6,8 @@ def pickle_to_file(obj, filename):
     :param obj: object to write to file
     :param filename: the file to write to
     """
-    outfile = open(filename, 'wb')
-    pickle.dump(obj, outfile)
-    outfile.close()
+    with open(filename, 'wb') as outfile:
+        pickle.dump(obj, outfile)
 
 
 def pickle_from_file(filename):
@@ -16,7 +15,5 @@ def pickle_from_file(filename):
     :param filename: file to read from
     :return: the object in file
     """
-    infile = open(filename, 'rb')
-    data = pickle.load(infile)
-    infile.close()
-    return data
+    with open(filename, 'rb') as infile:
+        return pickle.load(infile)
