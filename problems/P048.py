0574d9872311baa5cedd6facf3493fee78f81138
YoavCarmel <carmel.yoav@gmail.com>
2021-12-31 23:40:42 +0200

    typos

--- problems/P048.py
+++ problems/P048.py
@@ -3,5 +3,5 @@ def ans():
     limit: int = 1000
     ten_ten: int = 10 ** 10
     for i in range(1, limit + 1):
-        s += pow(i, i, ten_ten)  # pow modulu
+        s += pow(i, i, ten_ten)  # pow modulo
     return s % ten_ten
