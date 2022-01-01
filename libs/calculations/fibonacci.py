9211a55e36d83ce813a1dd2b991e1360b0d3ff37
YoavCarmel <carmel.yoav@gmail.com>
2022-01-01 12:01:04 +0200

    improved 304 dramatically

--- libs/calculations/fibonacci.py
+++ libs/calculations/fibonacci.py
@@ -1,6 +1,11 @@
+from typing import List
+
 import numpy as np
+from tqdm import tqdm
 
 from libs.calculations.general import power_mat
+
+FIB_MAT = np.array([[1, 1], [1, 0]], dtype="object")  # the fibonacci matrix
 
 
 def fibonacci_number_by_index(index: int, *, mod=None):
@@ -9,17 +14,35 @@ def fibonacci_number_by_index(index: int, *, mod=None):
     :param index: index of wanted fibonacci number
     :param mod: mod on the result
     :return: the number
-
-    NOTE: cant use numpy here because numbers are too big
     """
     if index == 0:
         return 0
-    if index == 1 or index == 2:
-        return 1
-
-    fib_mat = np.array([[1, 1], [1, 0]])  # the fibonacci matrix
-    mat_n = power_mat(fib_mat, index - 2, mod=mod)
-    res = np.sum(mat_n[0])
+    mat_n = power_mat(FIB_MAT, index - 1, mod=mod)
+    res = np.sum(mat_n[1])
     if mod is not None:
         res = res % mod
     return res
+
+
+def fibonacci_numbers_by_indices(indices: List[int], *, mod=None):
+    """
+        calculated fast using matrix multiplication and power, in log(n) time
+        :param indices: indices of wanted fibonacci numbers
+        :param mod: mod on the result
+        :return: the number
+        """
+    nums = []
+    first_index = indices[0]
+    if first_index == 0:
+        raise Exception("dont handle 0 index")
+    mat = power_mat(FIB_MAT, first_index - 1, mod=mod)
+    nums.append(np.sum(mat[1]))
+    for prev, curr in zip(indices, indices[1:]):
+        power_diff = curr - prev
+        mat = mat @ power_mat(FIB_MAT, power_diff, mod=mod)
+        if mod is not None:
+            mat = mat % mod
+            nums.append(np.sum(mat[1]) % mod)
+        else:
+            nums.append(np.sum(mat[1]))
+    return nums
--- problems/P304.py
+++ problems/P304.py
@@ -1,14 +1,12 @@
 from sympy import nextprime
-from tqdm import tqdm
 
-from libs.calculations.fibonacci import fibonacci_number_by_index
+from libs.calculations.fibonacci import fibonacci_numbers_by_indices
 
 
 def ans():
-    s = 0
     p = nextprime(10 ** 14)
     m = 1234567891011
-    for _ in tqdm(range(10 ** 5)):
-        s += fibonacci_number_by_index(p, mod=m)
-        p = nextprime(p)
-    return s % m
+    primes = [p]
+    for _ in range(10 ** 5 - 1):
+        primes.append(nextprime(primes[-1]))
+    return sum(fibonacci_numbers_by_indices(primes, mod=m)) % m
