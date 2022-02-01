f8176b126117bc344484f1e3e2e4b1e5b5e5c4b5
YoavCarmel <carmel.yoav@gmail.com>
2022-02-02 01:02:19 +0200

    added profiling file

---
+++ main_functions/profiling.py
@@ -0,0 +1,13 @@
+from problems.P001 import ans
+
+"""
+choose the matching ans function by changing the imported file,
+and run using the two terminal lines:
+python -m cProfile -o profiling.prof main_functions/profiling.py
+snakeviz .\profiling.prof
+
+for problems that run too fast, the result may be non-sense
+"""
+
+if __name__ == '__main__':
+    ans()
